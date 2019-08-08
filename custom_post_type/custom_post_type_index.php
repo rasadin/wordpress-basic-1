f1067e029f54068c32170ba29e5283dd61cf4e53
Mohd. Rasadin <12105708+rasadin@users.noreply.github.com>
2019-08-08 15:28:17 +0600

    Update custom_post_type_index.php

--- custom_post_type/custom_post_type_index.php
+++ custom_post_type/custom_post_type_index.php
@@ -1,4 +1,24 @@
 <?php
+
+/**
+ * Admin Menu Builder main file
+ *
+ * @package Admin Menu Builder
+ */
+
+/*
+Plugin Name:  CustomPost&Others 
+Plugin URI:   http://imransayed.com/custom-post-tax-builder
+Description:  This plugin creates custom post and taxonomies
+Version:      1.0.0
+Author:       MOHD. RASADIN
+Author URI:   https://profiles.wordpress.org/gsayed786
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:  admin-menu-builder
+Domain Path:  /languages
+*/
+
 // Include custom_post_type_functions.php
 require_once 'inc/custom_post_type_functions.php';
 add_action('wp_enqueue_scripts', 'load_scripts');
